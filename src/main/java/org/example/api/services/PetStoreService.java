b6eea936d1a94e06481d2a630ff3a1b08f55bdc5
“Oleksandr <o.nelipa@comparus.de>
2025-04-17 22:46:34 +0300

    add logging to PetStoreService

--- src/main/java/org/example/api/services/PetStoreService.java
+++ src/main/java/org/example/api/services/PetStoreService.java
@@ -1,10 +1,13 @@
 package org.example.api.services;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
 
+@Log4j2
 public class PetStoreService extends BasePetService {
 
     public PetStoreService() {
@@ -13,6 +16,7 @@ public class PetStoreService extends BasePetService {
 
     @SuppressWarnings("unchecked")
     public Map<String, Integer> getStoreInventory() {
+        log.info("Send GET request to /store/inventory");
         return given(requestSpecification)
                 .when()
                 .get("/inventory")
