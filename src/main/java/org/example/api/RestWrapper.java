c44b4fb5194cf3cdd971e9a5cd26a26b35de6bd3
“Oleksandr <o.nelipa@comparus.de>
2025-04-17 22:32:44 +0300

    make fluent getter for services

--- src/main/java/org/example/api/RestWrapper.java
+++ src/main/java/org/example/api/RestWrapper.java
@@ -1,9 +1,11 @@
 package org.example.api;
 
 import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.example.api.service.PetStoreService;
 
 @Getter
+@Accessors(fluent = true)
 public class RestWrapper {
 
     private final PetStoreService petStoreService;
