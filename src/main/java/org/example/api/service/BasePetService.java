61c0807fe56f1f3b51132bf1511d597035cf1003
“Oleksandr <o.nelipa@comparus.de>
2025-04-17 22:30:43 +0300

    implement BasePetService

---
+++ src/main/java/org/example/api/service/BasePetService.java
@@ -0,0 +1,22 @@
+package org.example.api.service;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.http.ContentType.JSON;
+import static org.example.config.ConfigurationManager.configuration;
+
+public abstract class BasePetService {
+
+    protected final RequestSpecification requestSpecification;
+
+    public BasePetService() {
+        requestSpecification = new RequestSpecBuilder()
+                .setContentType(JSON)
+                .setBaseUri(configuration().baseUri())
+                .setBasePath(pathToResource())
+                .build();
+    }
+
+    public abstract String pathToResource();
+}
